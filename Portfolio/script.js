e9687ab492bd8be370f91747daf8f709c00f8ff6
Ashwin <akgirach5@gmail.com>
2024-07-02 20:10:28 +0500

    kinger

---
+++ Portfolio/script.js
@@ -0,0 +1,35 @@
+
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click', function(e) {
+  document.querySelector('body').classList.toggle('mobile-nav-active');
+  this.classList.toggle('fa-xmark')
+})
+
+var typed = new Typed(".auto-input",{
+  strings: ['any', 'Front-End Developer!', 'Back-End Developer!', 'Freelancer!', 'Youtuber!'],
+  typedspeed: 100,
+  backspeed: 100,
+  backDelay: 2000,
+  loop: true,
+})
+
+
+let navLinks = document.querySelectorAll('nav ul li a')
+
+
+let sections = document.querySelectorAll('section')
+
+window.addEventListener('scroll', function (){
+  const scrollPos = window.scrollY + 20
+  sections.forEach(section => {
+    if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+        if(section.getAttribute('id') == link.getAttribute('herf').substring(1)) {
+          link.classList.add('active')
+        }
+      });
+    }
+  });
+});
